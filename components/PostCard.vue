6b662bc11335e644e538bd3177433485a87828da
wadeen <meister.vvdo@gmail.com>
2023-02-06 23:28:39 +0900

    Add: day.jsインストール

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,3 +1,8 @@
+<script setup>
+import { dateToString } from "/utils/dateToString";
+const props = defineProps({ post: Object });
+</script>
+
 <template>
   <div>
     <NuxtLink :to="`/post/${props.post.sys.id}`" class="group h-48 md:h-64 xl:h-96 flex flex-col bg-gray-100 rounded-lg shadow-lg overflow-hidden relative">
@@ -6,17 +11,13 @@
       <div class="bg-gradient-to-t from-gray-800 md:via-transparent to-transparent absolute inset-0 pointer-events-none"></div>
 
       <div class="relative p-4 mt-auto">
-        <span class="block text-gray-200 text-sm">April 07, 2021</span>
+        <span class="block text-gray-200 text-sm"> {{ dateToString(props.post.fields.createdAt, "YYYY/MM/DD") }}</span>
         <h2 class="text-white text-xl font-semibold transition duration-100 mb-2">
           {{ props.post.fields.title }}
         </h2>
 
-        <span class="text-indigo-300 font-semibold">Read more</span>
+        <span class="text-indigo-300 font-semibold">もっと見る...</span>
       </div>
     </NuxtLink>
   </div>
 </template>
-
-<script setup>
-const props = defineProps({ post: Object });
-</script>
